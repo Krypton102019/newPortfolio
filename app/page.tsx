3c48f569393dd0655516ec0146b862d537b7240c
Krypton102019 <88681250+Krypton102019@users.noreply.github.com>
2024-07-07 19:50:29 +0630

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
 import Approach from "@/components/Approach"
 import Footer from "@/components/Footer";
-import { FaUser, FaUsers } from "react-icons/fa6";
+import { FaNewspaper, FaUser, FaUsers } from "react-icons/fa6";
 export default async function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
@@ -17,7 +17,7 @@ export default async function Home() {
         navItems={[
           {name: "Home", link: "/", icon: <FaHome/>},
           {name:"About",link:"/#about",icon:<FaUser/>},
-          {name:"Projects",link:"/#projects",icon:<FaUser/>},
+          {name:"Projects",link:"/#projects",icon:<FaNewspaper/>},
           {name:"Reviews",link:"/#testimonials",icon:<FaUsers/>}
         ]}
         />
