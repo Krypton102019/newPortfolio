2ebc8ff5496647de4de8574cfb12987285369896
Krypton102019 <shayshady15@gmail.com>
2024-07-07 18:52:04 +0630

    first commit

---
+++ data/index.ts
@@ -0,0 +1,225 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a JS Animation library",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Marketing Agency Website",
+      des: "Lorem Media is a social media agency specializing in social media management, branding, and content marketing. They offer tailored solutions to enhance brand visibility and engagement.",
+      img: "/Marketing-New.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://goofy-visvesvaraya-641640.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "Ak Phyaw Sayar - Burmese cafe",
+      des: "Ak Phyaw Sayar is a Burmese restaurant offering fresh ingredients and easy-to-follow recipes for traditional dishes. Enjoy convenient delivery and the joy of homemade meals with their pre-portioned ingredients.",
+      img: "/FoodWebsiteMockUp.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://nervous-borg-4aa945.netlify.app/",
+    },
+    {
+      id: 3,
+      title: "Exchange Calculator",
+      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+      img: "/ExChangeCal.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://tranquil-lokum-6a4045.netlify.app/",
+    },
+    {
+      id: 4,
+      title: "Admin Dashboard Website",
+      des: "The Admin Dashboard Website offers a comprehensive platform for administrators to manage and monitor various aspects of their operations. It includes features such as user management, data analytics, and customizable reports to enhance efficiency and decision-making.",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://adoring-agnesi-12035a.netlify.app/",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "I had the pleasure of working with Erwin on a complex web application for Web Solutions LLC. As a Next.js Junior Developer, Erwin demonstrated exceptional technical skills and a keen eye for detail. His ability to quickly understand project requirements and deliver high-quality code was impressive. He communicated effectively and was always open to feedback, making the development process smooth and efficient. I highly recommend Erwin for any development team.",
+      name: "Ricky",
+      title: "CEO, Innovate Tech Solutions",
+      img:"/ricky.jpg"
+    },
+    {
+      quote:
+        "Erwin was an invaluable asset to our project at Digital Dynamics. His proficiency in Next.js and his problem-solving abilities stood out throughout the development process. He approached every challenge with enthusiasm and a positive attitude, contributing significantly to the project's success. His collaborative spirit and dedication to continuous improvement make him an excellent Junior Developer. We look forward to working with Erwin again.",
+      name: "Calissa",
+      title: "CEO, Logistic Company",
+      img:"/Calissa.jpg"
+    },
+    {
+      quote:
+        "Working with Erwin was a fantastic experience. As a Next.js Junior Developer, he brought fresh perspectives and innovative solutions to our team at Tech Innovators. His ability to learn quickly and adapt to new challenges was remarkable. Erwin consistently delivered high-quality work and went above and beyond to ensure project success. His strong communication skills and collaborative approach made him a pleasure to work with.",
+      name: "Phyo Khant Nyi",
+      title: "Owner, La Petite Boulangerie",
+      img:"/PhyoKhantNyi.jpg"
+    },
+    {
+      quote:
+        "Erwin joined our team at Creative Coders Inc. as a Next.js Junior Developer and made an immediate impact. His technical expertise and attention to detail were evident from day one. He worked diligently to meet project deadlines and always maintained a high standard of quality. Erwin's proactive approach and willingness to take on new challenges were highly appreciated. I highly recommend him for any development role.",
+      name: "Kaung Khant",
+      title: "Owner, Panda Fruit",
+      img:"KaungKhant.jpg"
+    },
+    {
+      quote:
+        "Erwin is an exceptional Next.js Junior Developer. At Innovate Tech Solutions, he played a crucial role in developing our latest project. His ability to write clean, efficient code and his dedication to learning and improving were impressive. Erwin's positive attitude and strong work ethic made him an integral part of our team. He consistently went the extra mile to ensure the project's success. I highly recommend Erwin for any future opportunities.",
+      name: "Adam Wong",
+      title: "Lead Developer, Wong Tech Solutions",
+      img:"AdamWong.jpg"
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev - JSM Tech",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      link:"https://github.com/Krypton102019", 
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      link:"https://www.instagram.com/yewinmyat17?igsh=aDJreWk2czh1dHdm",
+      img: "/insta.svg",
+    },
+    {
+      id: 3,
+      link:"",
+      img: "/link.svg",
+    },
+  ];
